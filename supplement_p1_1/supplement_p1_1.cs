04269a8cbf9b0f6c2d625f1c4bdf555a09ad7836
Brianna Moore <briannamoore@MacBook-Air.local>
2025-01-29 23:53:37 -0500

    doc: added documentation for area of rectangle

--- supplement_p1_1/supplement_p1_1.cs
+++ supplement_p1_1/supplement_p1_1.cs
@@ -2,8 +2,20 @@
 
 public class supplement_p1_1
 {
+    /// <summary>
+    /// Returns the area of a rectangle. If the lenght is equal to width, 
+    /// calculation is done as length * length instead.
+    /// </summary>
+    /// <param name="length">Lenght of the Rectangle.</param>
+    /// <param name="width">Width of the rectangle.</param>
+    /// <returns>Area of the rectangle.</returns>
     public static double AreaOfRectangle(double length, double width) {
-        return length * width;
+        if (length == width){
+            return length * length; //or double.Pow(length, 2.0)
+        } else{
+            return length * width;
+        }
+        
 
     }
     public static double AreaOfCircle(double radius) {
