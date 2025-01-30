59539395971dfd9ad57bc178ce86f80f2935da5a
Brianna Moore <briannamoore@MacBook-Air.local>
2025-01-29 23:44:25 -0500

    test: added test for rectangle area with same lenght and width

--- supplement_p1_1.Tests/UnitTest1.cs
+++ supplement_p1_1.Tests/UnitTest1.cs
@@ -3,8 +3,13 @@ namespace supplement_p1_1.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void ShouldCalculateAreaOfRectangleWithDifferentLengthAndWidth()
     {
-
+        Assert.True(50.0 == supplement_p1_1.AreaofRectangle(5.0, 10.0));
+    }
+      [Fact]
+    public void ShouldCalculateAreaOfRectangleWithSameLengthAndWidth()
+    {
+        Assert.True(25.0 == supplement_p1_1.AreaofRectangle(5.0, 5.0));
     }
 }
