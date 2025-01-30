d33df3e3651c2cba4d51ce5fdc34341990e4c88c
Brianna Moore <briannamoore@MacBook-Air.local>
2025-01-29 23:58:35 -0500

    test: AreaOfCircle test for unit circle

--- supplement_p1_1.Tests/UnitTest1.cs
+++ supplement_p1_1.Tests/UnitTest1.cs
@@ -12,4 +12,9 @@ public class UnitTest1
     {
         Assert.True(25.0 == supplement_p1_1.AreaofRectangle(5.0, 5.0));
     }
+      [Fact]
+    public void ShouldCalculateAreaOfUnitCircle()
+    {
+        Assert.True(double.Pi== supplement_p1_1.AreaCircle(1.0));
+    }
 }
