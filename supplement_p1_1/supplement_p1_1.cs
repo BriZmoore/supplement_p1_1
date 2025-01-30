320e23f5198852757e8e131e36fa6f6b48af3bd8
Brianna Moore <briannamoore@MacBook-Air.local>
2025-01-29 23:39:40 -0500

    feat: implemented AreaOfRectangle for base case

--- supplement_p1_1/supplement_p1_1.cs
+++ supplement_p1_1/supplement_p1_1.cs
@@ -1,6 +1,17 @@
 ﻿namespace supplement_p1_1;
 
-public class Class1
+public class supplement_p1_1
 {
+    public static double AreaOfRectangle(double length, double width) {
+        return length * width;
+
+    }
+    public static double AreaOfCircle(double radius) {
+        throw new NotImplementedException();
+    }
+    public static double AreaOfTriangle(double triangleBase, double triangleHeight ) {
+        throw new NotImplementedException();
+    }
+    
 
 }
